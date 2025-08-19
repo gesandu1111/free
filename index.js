a2f9685e446001aade7e6c324b84a0b28b538c18
gesandu1111 <gesandusanmira@gmail.com>
2025-08-19 20:38:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-import paircodeHandler from "./paircodeHandler.js";
+const { default: makeWASocket } = require("@adiwajshing/baileys");
+const { writeFileSync, readFileSync } = require("fs");
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+async function startBot() {
+    const sock = makeWASocket();
+    sock.ev.on("messages.upsert", (msg) => {
+        console.log(msg);
+    });
+}
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, "public")));
-app.get("/code", paircodeHandler);
-
-app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
-});
+startBot();
